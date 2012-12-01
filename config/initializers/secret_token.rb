e57fc622c7eeaefcf34cf7fe4d526d69f97ef392
ChoJaewoo <chjw102@gmail.com>
2012-12-01 14:59:13 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cukis::Application.config.secret_token = '7b6587698607e480576fdba680090cbd428fc215e81193cc51e5e1ef66bbe6b4d5637ca96d666a7d577e4422c9fba1ff2f91a376594ff0248e11d851a6d6d4a3'
