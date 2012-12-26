c98fb67cfa559df67665df0933079392e642dc11
Junyoung park <jun@ubuntu.(none)>
2012-12-26 17:45:37 +0900

    June Front End

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'ca2cf62b2e550ebf24291d7657a0553d834f3a157ac9e7d36c7a326e431dc0c11084cc9dcbbedf9fdac06ecb1cb39db0141442c7b14364041ae0d61dae8d7a24';
