8f8608a050e0df33ec67127858c2705ec16b022b
ChoJaewoo <chjw102@gmail.com>
2013-01-05 12:23:59 +0900

    change dayclass client_id & width 960

--- app/models/dayclass.rb
+++ app/models/dayclass.rb
@@ -1,7 +1,8 @@
 class Dayclass < ActiveRecord::Base
 	scope :approve, where(approve: true)
 
-	attr_accessible :title, :content, :fee, :minnumber, :maxnumber, :fromdate, :enddate,:fromtime, :endtime, :doc, :place, :approve, :coverpic
+	attr_accessible :title, :content, :fee, :minnumber, :maxnumber, :fromdate, :enddate,:fromtime, :endtime, :doc, :place, :approve, :coverpic,
+									:client_id
 	mount_uploader :coverpic, ImageUploader
 	has_many :replies, as: :repliable
 	belongs_to :client
