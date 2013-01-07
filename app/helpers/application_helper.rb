b051fd11d9cf094e69db6db6585e49041a7a30b5
ChoJaewoo <chjw102@gmail.com>
2013-01-07 12:21:11 +0900

    privacy global info add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
 	def privacy
 		@privacy = Globalinfo.find(2).content.html_safe
 	end
+
+	def introduce
+		@introduce = Globalinfo.find(3).content.html_safe
+	end
+
+	def question
+		@question = Globalinfo.find(4).content.html_safe
+	end
 end
