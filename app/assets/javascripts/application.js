cf608d24ab40700ad4a6fb5e774140779641be90
ChoJaewoo <chjw102@gmail.com>
2013-01-08 17:38:09 +0900

    bootstrap min change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap.min
 //= require bootstrap-datepicker/core
 //= require bootstrap-timepicker
 //= require wmd/wmd
